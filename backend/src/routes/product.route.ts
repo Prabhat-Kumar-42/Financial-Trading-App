4246815b3a1d369746b06a1b7349dc7ec2bc6eea
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-27 11:46:19 +0530

    feat: add product routes with authentication middleware

---
+++ backend/src/routes/product.route.ts
@@ -0,0 +1,9 @@
+import { getProduct, listProducts } from "@/controllers/product.controller.js";
+import { authenticate } from "@/middlewares/auth.middleware.js";
+import { Router } from "express";
+
+export const productRouter = Router();
+
+productRouter.get("/", authenticate, listProducts);
+productRouter.get("/:id", authenticate, getProduct);
+
