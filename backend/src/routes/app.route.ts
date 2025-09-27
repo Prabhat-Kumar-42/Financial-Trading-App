fd0d2bd73cc44b544c77a33c8a1c9e48f9fc130a
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-27 11:46:49 +0530

    feat: add product routes to appRouter

--- backend/src/routes/app.route.ts
+++ backend/src/routes/app.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authRouter } from "./auth.route.js";
+import { productRouter } from "./product.route.js";
 
 // /src/routes/auth.route.ts
 export const appRouter = Router();
 
 appRouter.use('/auth', authRouter);
-
+appRouter.use('/products', productRouter);
