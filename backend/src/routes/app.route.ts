ba923eb4b2613bfc45e5c0f0c261dc15a2564bf9
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-27 13:29:38 +0530

    feat(routes): add transaction, portfolio, and watchlist routes to appRouter

--- backend/src/routes/app.route.ts
+++ backend/src/routes/app.route.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { authRouter } from "./auth.route.js";
 import { productRouter } from "./product.route.js";
+import { portfolioRouter } from "./portfolio.route.js";
+import { transactionRouter } from "./transaction.route.js";
+import { watchListRouter } from "./watchlist.route.js";
 
 // /src/routes/auth.route.ts
 export const appRouter = Router();
 
 appRouter.use('/auth', authRouter);
 appRouter.use('/products', productRouter);
+appRouter.use("/transactions", transactionRouter);
+appRouter.use("/portfolio", portfolioRouter);
+appRouter.use("/watchlist", watchListRouter);
+
