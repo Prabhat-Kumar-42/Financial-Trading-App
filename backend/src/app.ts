7115ba90991d2a96d768ba3bb043c11a87be9491
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-27 07:21:53 +0530

    feat: add error handling middleware to app setup
    - Add nonExistingRoutesErrorHandler for handling 404 errors for non-existing routes
    - Include jwtErrorHandler to manage specific JWT-related errors (expired, invalid, inactive tokens)
    - Implement globalErrorHandler to catch all other errors and send consistent error responses
    - Improve error handling flow by centralizing all error handlers in the app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { nonExistingRoutesErrorHandler } from "./error-handlers/non-existing-route.error-handler.js";
+import { jwtErrorHandler } from "./error-handlers/jwt.error-handler.js";
+import { globalErrorHandler } from "./error-handlers/global.error-handler.js";
 
 // /src/app.ts
 dotenv.config();
@@ -11,3 +14,11 @@ app.use(express.json());
 
 app.get("/healthz", (req, res) => res.send("Mini Finance API running"));
 
+// Handle non-existing routes
+app.use(nonExistingRoutesErrorHandler);
+
+// JWT error handling
+app.use(jwtErrorHandler);
+
+// Global error handler
+app.use(globalErrorHandler);
