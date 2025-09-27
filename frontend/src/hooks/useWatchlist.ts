f1e99224f1af14c97e092f7a15a7648f8c82685d
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-28 00:12:14 +0530

    lint: fix

--- frontend/src/hooks/useWatchlist.ts
+++ frontend/src/hooks/useWatchlist.ts
@@ -1,7 +1,7 @@
 "use client";
 import { useWatchlistContext } from "@/contexts/WatchlistContext";
-// /src/hooks/useWatchList.ts
 
+// /src/hooks/useWatchList.ts
 export function useWatchlist() {
   const { add, remove, loading } = useWatchlistContext();
   return { add, remove, loading };
